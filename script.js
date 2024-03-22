5d3435145f89475268febb03f1364f1df8040afd
kannanATexcelforte <kannan@excelforte.com>
2024-03-22 11:22:26 +0530

    22.03.2024-frf
    completed

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
 'use strict';
 
-const oneWord = function (str) {
-    return str.replace(/ /g, '').toLowerCase();
+const greet = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    }
 }
 
-const upperFirstWord = function (str) {
-    const [first, ...others] = str.split(' ');
-    return [first.toUpperCase(), ...others].join(' ');
-}
+const greeterHey = greet('Hey');
+console.log(greeterHey);
+greeterHey('Jonas');
+greeterHey('Steven');
 
-// higher order function
-const transformer = function (str, fn) {
-    console.log(`Original string: ${str}`);
-    console.log(`Transformed string: ${fn(str)}`);
-    console.log(`Transformed by: ${fn.name}`);
-}
+greet('Hello')('Jonas');
 
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
+// challenge
 
-// JS uses callbacks all the time
-const high5 = function () {
-    console.log('Hi');
-}
-
-document.body.addEventListener('click', high5);
-
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('Hi')('Jonas');

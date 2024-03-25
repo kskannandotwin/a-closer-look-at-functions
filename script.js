0a693e70119acb3aba4869eb0d08691b2ab5df51
kannanATexcelforte <kannan@excelforte.com>
2024-03-25 12:28:18 +0530

    25.03.2024-mce
    completed

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 'use strict';
 
-const secureBooking = function () {
-    let passengerCount = 0;
+// example 1
+let f;
 
-    return function () {
-        passengerCount++;
-        console.log(`${passengerCount} passengers`);
+const g = function() {
+    const a = 23;
+    f = function() {
+        console.log(a * 2);
+    }
+};
+
+const h = function() {
+    const b = 777;
+    f = function() {
+        console.log(b * 2);
     }
 }
 
-const booker = secureBooking();
+g();
+f();
+console.dir(f);
 
-booker();
-booker();
-booker();
+// re-assigning f function
+h();
+f();
+console.dir(f);
 
-console.dir(booker);
+// example 2
+const boardPassengers = function(n, wait) {
+    const perGroup = n / 3;
+
+    setTimeout(function() {
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);

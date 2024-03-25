7ccac8b2b5f1e4128c7581168746e0925a5c1cf4
kannanATexcelforte <kannan@excelforte.com>
2024-03-25 15:01:00 +0530

    25.03.2024-cc#2
    completed

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
 'use strict';
 
-// example 1
-let f;
+// Coding Challenge #2
 
-const g = function() {
-    const a = 23;
-    f = function() {
-        console.log(a * 2);
-    }
-};
+/* 
+This is more of a thinking challenge than a coding challenge
 
-const h = function() {
-    const b = 777;
-    f = function() {
-        console.log(b * 2);
-    }
-}
+Take the IIFE below and at the end of the function, attach an event listener that changes the
+color of the selected h1 element ('header') to blue, each time the BODY element is clicked.
+Do NOT select the h1 element again!
 
-g();
-f();
-console.dir(f);
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need.
+Think about WHEN exactly the callback function is executed, and what that means for the variables
+involved in this example.
 
-// re-assigning f function
-h();
-f();
-console.dir(f);
+GOOD LUCK
+*/
 
-// example 2
-const boardPassengers = function(n, wait) {
-    const perGroup = n / 3;
+(function() {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
 
-    setTimeout(function() {
-        console.log(`We are now boarding all ${n} passengers`);
-        console.log(`There are 3 groups, each with ${perGroup} passengers`);
-    }, wait * 1000);
-
-    console.log(`Will start boarding in ${wait} seconds`);
-};
-
-const perGroup = 1000;
-boardPassengers(180, 3);
+    document.body.addEventListener('click', function() {
+        header.style.color = 'blue';
+    })
+})();
